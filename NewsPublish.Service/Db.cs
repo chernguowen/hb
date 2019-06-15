b6617b5490c2f41c489b729a7c99511b30865505
chernguowen <369001990@qq.com>
2019-06-15 18:40:27 +0800

    xml数据库改好

--- NewsPublish.Service/Db.cs
+++ NewsPublish.Service/Db.cs
@@ -5,22 +5,26 @@ using System;
 using System.Collections.Generic;
 using System.Text;
 using Microsoft.AspNetCore.Hosting;
-
+using System.IO;
 
 namespace NewsPublish.Service
 {
   public  class Db:DbContext
     {
-        public Db() { }
         private IHostingEnvironment _host;
+        public Db(IHostingEnvironment host) {
+            this._host = host;
+        }
+       
         protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder) {
             //以前的连接法
             //base.OnConfiguring(optionsBuilder);
             //optionsBuilder.UseSqlServer("Data Source=127.0.0.1;Initial Catalog=NewsPublish; ID=sa;Password=cgw", b => b.UseRowNumberForPaging());
             //用xml连接数据库
-            XDocument doc = XDocument.Load(_host.WebRootPath+"\\Config.xml");
-            string dbcontent = doc.Element("Root").Element("Name").Value;
-            optionsBuilder.UseSqlServer(@dbcontent);
+            string DbPath = _host.WebRootPath;          
+            XDocument doc = XDocument.Load(DbPath+"\\Config.xml");            
+            string DbConnection = doc.Element("Root").Element("Name").Value;
+            optionsBuilder.UseSqlServer(@DbConnection);
             // optionsBuilder.UseSqlServer(@"Server=.;Data Source=127.0.0.1;uid=sa;pwd=cgw;DataBase=NewsPublish");
         }
         protected override void OnModelCreating(ModelBuilder modelBuilder)
